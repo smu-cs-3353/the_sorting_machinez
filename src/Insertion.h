664311082c553ec264cce701636817789462fa36
Kiots Kaneshirom <kkaneshirom@gmail.com>
2022-03-02 11:25:11 -0600

    did insertion sort algorithm

--- src/Insertion.h
+++ src/Insertion.h
@@ -1,1 +1,24 @@
 
+#ifndef INSERTION_H__
+#define INSERTION_H__
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+class Insertion{
+	
+public:
+
+	Insertion(string);
+	
+	
+		
+};
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "Insertion.h"
 
-int main(int argc, char argv**){
 
+//remember to add '\n' to end of data for count
+
+int main(int argc, char** argv){
+
+	Insertion i(argv[1]);
+	
+	return 0;
 }
