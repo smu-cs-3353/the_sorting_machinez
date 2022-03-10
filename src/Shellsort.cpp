8e456d40b5a7b00c5818fe15a72dc23c47289047
Kiots Kaneshirom <kkaneshirom@gmail.com>
2022-03-09 23:06:14 -0600

    finished shellsort

--- src/Shellsort.cpp
+++ src/Shellsort.cpp
@@ -1,6 +1,36 @@
 #include "Shellsort.h"
+// referenced geeksforgeeks: https://www.geeksforgeeks.org/shellsort/
 
-Shellsort::Shellsort(string directory) : Sort(directory){
+Shellsort::Shellsort(string directory) : Sort(directory)
+{
+    cout << "-----shellsort!-----" << endl;
+    for (int s : input_vector) // test print
+        cout << s << endl;
 
-    
+    shellsorter();
+
+    cout << "-----shellsort done-----" << endl;
+    for (int s : input_vector) // test print
+        cout << s << endl;
+}
+
+void Shellsort::shellsorter()
+{
+
+    int size = input_vector.size();
+
+    for (int gap = size / 2; gap > 0; gap /= 2)
+    {
+
+        for (int i = gap; i < size; i ++)
+        {
+            int j = i;
+
+            while (j >= gap && input_vector.at(j) < input_vector.at(j - 1))
+            {
+                swap(input_vector.at(j), input_vector.at(j - gap));
+                j -= gap;
+            }
+        }
+    }
 }
